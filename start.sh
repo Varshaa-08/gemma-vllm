4a779a3e80a470664e30c5f9b01fe6e6ff38d4db
Varshaa-08 <sjvarshaa8@gmail.com>
2025-04-04 17:41:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 # Ensure script is executable
 chmod +x start.sh
 
-# Render assigns a dynamic port, so we use $PORT
+# Render assigns a dynamic port
 gunicorn -w 1 -k uvicorn.workers.UvicornWorker backend:app --bind 0.0.0.0:$PORT
+
