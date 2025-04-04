03be577dbc2fbf7b1066515433f8b93b2545b98b
Varshaa-08 <sjvarshaa8@gmail.com>
2025-04-04 17:23:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Ensure the script is executable
+# Ensure script is executable
 chmod +x start.sh
 
-# Run FastAPI with Gunicorn (use only 1 worker to avoid CPU overload)
-gunicorn -w 1 -k uvicorn.workers.UvicornWorker backend:app --bind 0.0.0.0:8000
+# Render assigns a dynamic port, so we use $PORT
+gunicorn -w 1 -k uvicorn.workers.UvicornWorker backend:app --bind 0.0.0.0:$PORT
