c0e0a926893e784197cfa5b1f8e5b8c6c696af83
Varshaa-08 <sjvarshaa8@gmail.com>
2025-04-04 15:34:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Ensure script is executable
+# Ensure the script is executable
 chmod +x start.sh
 
-# Run FastAPI with Gunicorn using Uvicorn worker
+# Run FastAPI with Gunicorn (use only 1 worker to avoid CPU overload)
 gunicorn -w 1 -k uvicorn.workers.UvicornWorker backend:app --bind 0.0.0.0:8000
